8faa5da1aa4561e01731427c6c19111685e927ec
Florian Munz <surf@theflow.de>
2008-11-26 17:33:27 +0100

    let's use some contexts

--- test/place_mapping_test.rb
+++ test/place_mapping_test.rb
@@ -4,81 +4,68 @@ class PlaceMappingTest < Test::Unit::TestCase
 
   place_xml = File.read(File.dirname(__FILE__) + '/fixtures/place.xml')
 
-  test 'should parse a single place' do
-    place = Qype::Place.parse(place_xml, :single => true)
-    assert_not_nil place
-  end
+  context 'A single place' do
+    before do
+      @place = Qype::Place.parse(place_xml, :single => true)
+    end
 
-  test 'should have basic elements' do
-    place = Qype::Place.parse(place_xml, :single => true)
-    assert_equal 'Mr. Kebab', place.title
-    assert_equal '040 437798', place.phone
-    assert_equal 4.95, place.average_rating
-    assert_equal '53.5564,9.96239', place.point
-  end
+    test 'should have basic elements' do
+      assert_equal 'Mr. Kebab', @place.title
+      assert_equal '040 437798', @place.phone
+      assert_equal 4.95, @place.average_rating
+      assert_equal '53.5564,9.96239', @place.point
+    end
 
-  test 'should include a image element with three thumbnail sizes' do
-    place = Qype::Place.parse(place_xml, :single => true)
+    test 'should include a image element with three thumbnail sizes' do
+      image = @place.image
+      assert_not_nil image
+      assert_equal "http://assets1.qype.com/uploads/photos/0017/4291/IMG_2092_mini.JPG?1208428301", image.small
+      assert_equal "http://assets2.qype.com/uploads/photos/0017/4291/IMG_2092_thumb.JPG?1208428303", image.medium
+      assert_equal "http://assets3.qype.com/uploads/photos/0017/4291/IMG_2092_gallery.JPG?1208428300", image.large
+    end
 
-    image = place.image
-    assert_not_nil image
-    assert_equal "http://assets1.qype.com/uploads/photos/0017/4291/IMG_2092_mini.JPG?1208428301", image.small
-    assert_equal "http://assets2.qype.com/uploads/photos/0017/4291/IMG_2092_thumb.JPG?1208428303", image.medium
-    assert_equal "http://assets3.qype.com/uploads/photos/0017/4291/IMG_2092_gallery.JPG?1208428300", image.large
-  end
+    test 'should include a address element' do
+      address = @place.address
+      assert_not_nil address
+      assert_equal 'Thadenstrasse', address.street
+      assert_equal '22767', address.postcode
+      assert_equal '1', address.housenumber
+      assert_equal 'Hamburg', address.city
+    end
 
-  test 'should include a address element' do
-    place = Qype::Place.parse(place_xml, :single => true)
+    test 'should include multiple category elements' do
+      categories = @place.categories
+      assert_equal 2, categories.size
+      assert_equal "Döner und Griechischer Imbiss", categories[0].title
+      assert_equal "Türkische Restaurants", categories[1].title
+    end
 
-    address = place.address
-    assert_not_nil address
-    assert_equal 'Thadenstrasse', address.street
-    assert_equal '22767', address.postcode
-    assert_equal '1', address.housenumber
-    assert_equal 'Hamburg', address.city
-  end
+    test 'should include links to reviews in multiple languages' do
+      review_links = @place.links.select { |link| link.rel == 'http://schemas.qype.com/reviews' }
+      assert_equal 5, review_links.size
 
-  test 'should include multiple category elements' do
-    place = Qype::Place.parse(place_xml, :single => true)
+      german_link = review_links.find { |link| link.hreflang == 'de' }
+      assert_equal 'http://api.qype.com/v1/places/7019/reviews/de', german_link.href
+      assert_equal 40, german_link.count
+    end
 
-    categories = place.categories
-    assert_equal 2, categories.size
-    assert_equal "Döner und Griechischer Imbiss", categories[0].title
-    assert_equal "Türkische Restaurants", categories[1].title
-  end
+    test 'should include a link to the locator' do
+      locator_link = @place.links.find { |link| link.rel == 'http://schemas.qype.com/locator' }
+      assert_not_nil locator_link
+      assert_equal 'Hamburg', locator_link.title
+    end
 
-  test 'should include links to reviews in multiple languages' do
-    place = Qype::Place.parse(place_xml, :single => true)
+    test 'should include a link to assets for the place' do
+      assets_link = @place.links.find { |link| link.rel == 'http://schemas.qype.com/assets' }
+      assert_not_nil assets_link
+    end
 
-    review_links = place.links.select { |link| link.rel == 'http://schemas.qype.com/reviews' }
-    assert_equal 5, review_links.size
+    test 'should fetch reviews in a specific language' do
+      client = mock_client(File.dirname(__FILE__) + '/fixtures/reviews.xml')
 
-    german_link = review_links.find { |link| link.hreflang == 'de' }
-    assert_equal 'http://api.qype.com/v1/places/7019/reviews/de', german_link.href
-    assert_equal 40, german_link.count
-  end
-
-  test 'should include a link to the locator' do
-    place = Qype::Place.parse(place_xml, :single => true)
-
-    locator_link = place.links.find { |link| link.rel == 'http://schemas.qype.com/locator' }
-    assert_not_nil locator_link
-    assert_equal 'Hamburg', locator_link.title
-  end
-
-  test 'should include a link to assets for the place' do
-    place = Qype::Place.parse(place_xml, :single => true)
-
-    assets_link = place.links.find { |link| link.rel == 'http://schemas.qype.com/assets' }
-    assert_not_nil assets_link
-  end
-
-  test 'should fetch reviews in a specific language' do
-    place = Qype::Place.parse(place_xml, :single => true)
-    client = mock_client(File.dirname(__FILE__) + '/fixtures/reviews.xml')
-
-    reviews = place.reviews(client, 'de')
-    assert_equal 10, reviews.size
+      reviews = @place.reviews(client, 'de')
+      assert_equal 10, reviews.size
+    end
   end
 
 end
--- test/review_mapping_test.rb
+++ test/review_mapping_test.rb
@@ -4,55 +4,44 @@ class ReviewMappingTest < Test::Unit::TestCase
 
   review_xml = File.read(File.dirname(__FILE__) + '/fixtures/review.xml')
 
-  test 'should parse a single review' do
-    review = Qype::Review.parse(review_xml, :single => true)
-    assert_not_nil review
-  end
+  context 'A single review' do
+    before do
+      @review = Qype::Review.parse(review_xml, :single => true)
+    end
 
-  test 'should have basic review elements' do
-    review = Qype::Review.parse(review_xml, :single => true)
+    test 'should have basic review elements' do
+      assert_equal 'de', @review.language
+      assert_equal 4, @review.rating
 
-    assert_equal 'de', review.language
-    assert_equal 4, review.rating
+      paragraph = /<p>/
+      assert_no_match paragraph, @review.content
 
-    paragraph = /<p>/
-    assert_not_nil review.content
-    assert_no_match paragraph, review.content
+      assert_match paragraph, @review.formatted_content
+    end
 
-    assert_not_nil review.formatted_content
-    assert_match paragraph, review.formatted_content
-  end
+    test 'should include tag elements' do
+      tags = @review.tags
+      assert_equal 8, tags.size
+      assert_equal "günstig", tags.first.term
+    end
 
-  test 'should include tag elements' do
-    review = Qype::Review.parse(review_xml, :single => true)
+    test 'should include a link to the locator' do
+      locator_link = @review.links.find { |link| link.rel == 'http://schemas.qype.com/locator' }
+      assert_not_nil locator_link
+      assert_equal 'Hamburg', locator_link.title
+    end
 
-    tags = review.tags
-    assert_equal 8, tags.size
-    assert_equal "günstig", tags.first.term
-  end
+    test 'should include a link to the place' do
+      place_link = @review.links.find { |link| link.rel == 'http://schemas.qype.com/place' }
+      assert_not_nil place_link
+      assert_equal 'Mr. Kebab', place_link.title
+    end
 
-  test 'should include a link to the locator' do
-    review = Qype::Review.parse(review_xml, :single => true)
-
-    locator_link = review.links.find { |link| link.rel == 'http://schemas.qype.com/locator' }
-    assert_not_nil locator_link
-    assert_equal 'Hamburg', locator_link.title
-  end
-
-  test 'should include a link to the place' do
-    review = Qype::Review.parse(review_xml, :single => true)
-
-    place_link = review.links.find { |link| link.rel == 'http://schemas.qype.com/place' }
-    assert_not_nil place_link
-    assert_equal 'Mr. Kebab', place_link.title
-  end
-
-  test 'should include a link to the user' do
-    review = Qype::Review.parse(review_xml, :single => true)
-
-    user_link = review.links.find { |link| link.rel == 'http://schemas.qype.com/user' }
-    assert_not_nil user_link
-    assert_equal 'Grobi', user_link.title
+    test 'should include a link to the user' do
+      user_link = @review.links.find { |link| link.rel == 'http://schemas.qype.com/user' }
+      assert_not_nil user_link
+      assert_equal 'Grobi', user_link.title
+    end
   end
 
 end
